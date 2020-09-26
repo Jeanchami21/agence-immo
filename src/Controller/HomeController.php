523063a4b1ab6ea4953d9aa9cbd9e9e222bfda03
Jeanchami21 <chamijean21@gmail.com>
2020-09-26 10:52:54 +0200

    installation de symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+    /**
+     *
+     * @var [environment]
+     */
+    private $twig;
+
+    public function __construct($twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function index(): Response
+    {
+        return new Response($this->twig->render('pages/home.html.twig'));
+    }
+}
